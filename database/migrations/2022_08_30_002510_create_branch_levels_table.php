acfc64400bb97df1ec1171d3e7f15c80dae60eec
Abderrahman Fodili <bestiony12373@gmail.com>
2022-08-30 03:00:20 +0200

    make main MCMs , migrate successfully

---
+++ database/migrations/2022_08_30_002510_create_branch_levels_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branch_levels', function (Blueprint $table) {
+            $table->foreignId("level_id")->constrained();
+            $table->foreignId("branch_id")->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branch_levels');
+    }
+};
---
+++ database/migrations/2022_08_30_003251_create_branch_level_subject_units_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('branch_level_subject_units', function (Blueprint $table) {
+            $table->foreignId('level_id')->constrained();
+            $table->foreignId('branch_id')->constrained();
+            $table->foreignId('subject_id')->constrained();
+            $table->foreignId('unit_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('branch_level_subject_units');
+    }
+};
